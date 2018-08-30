4d6780d05245c6dd70766db089d731e70d7cffc5
Terrance <tbarnett2020@gmail.com>
2018-08-30 13:07:18 -0400

    Refactered with action generators(Playground)

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,31 +1,45 @@
 import { createStore } from 'redux'
+
 /* To use redux we have to access createStore and its the thing that tracks our changing data over time. To use it we need a function and it gets called once right away. No state the first time its called so we use the default */
 
-//  Same as this below
-/*
-this.setState((prevState) => {
-   return prevState;
- }) 
- */
+// Action Generators  - functions that return action objects
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy: incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy: decrementBy
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+const setCount = ({count} = {}) => ({
+    type: 'SET',
+    count
+});
 
 //  No constructor function to set default state so we set it up here------------------->>>>>>>>
 const store = createStore((state = {count: 0}, action) => {
   switch (action.type) {
     case 'INCREMENT':
     // If the action.incrementBy is a number then put that if not default to 1
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+    //   const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
     return {
       // So now incrementBy can be 5 if its there but 1 by default ifs not. Makin it more dynamic
-      count: state.count + incrementBy
+      count: state.count + action.incrementBy
     };
     case 'DECREMENT':
-    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
     return {
-      count: state.count - decrementBy
+      count: state.count - action.decrementBy
     };
     case 'SET':
     return {
-      count: action.count
+      count: action.setBy
     };
     case 'RESET':
     return {
@@ -45,33 +59,19 @@ const unsubscribe = store.subscribe(() => {
 // Actions- an object that gets sent to the store
 
 // I'd like to increment the count
-store.dispatch( {
-  type: 'INCREMENT',
-  incrementBy: 5
-});
 
-store.dispatch( {
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-// I'd like to decrement the count
-
-store.dispatch( {
-  type: 'DECREMENT',
-  decrementBy: 10
-});
-
-store.dispatch( {
-  type: 'DECREMENT',
-});
+// Action generater 
+store.dispatch(incrementCount());
 
 // RESET count to 0
+store.dispatch(resetCount());
 
-store.dispatch( {
-  type: 'RESET'
-});
+// I'd like to decrement the count
+store.dispatch(decrementCount());
 
-store.dispatch( {
-  type: 'SET',
-  count: 101
-});
+store.dispatch(decrementCount({decrementBy: 10}));
+
+// Set to any number (101)
+store.dispatch(setCount({count: 101}));
