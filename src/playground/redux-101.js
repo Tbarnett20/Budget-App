3bf14ae9a522cb7b2a4a5e27ae711e5b98138e5d
Terrance <tbarnett2020@gmail.com>
2018-08-30 10:55:38 -0400

    Dispatched actions (Playground)

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -9,9 +9,52 @@ this.setState((prevState) => {
  */
 
 //  No constructor function to set default state so we set it up here------------------->>>>>>>>
-const store = createStore((state = {count: 0}) => {
-  return state;
+const store = createStore((state = {count: 0}, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+    return {
+      count: state.count + 1
+    };
+    case 'DECREMENT':
+    return {
+      count: state.count - 1
+    };
+    case 'RESET':
+    return {
+      count: state.count = 0
+    };
+    default: 
+     return state;
+  }
 });
 
 // getState() Method that returns the current states object
+console.log(store.getState())
+
+// Actions- an object that gets sent to the store
+
+// I'd like to increment the count
+store.dispatch( {
+  type: 'INCREMENT'
+});
+
+console.log(store.getState())
+// I'd like to decrement the count
+
+store.dispatch( {
+  type: 'DECREMENT'
+});
+console.log(store.getState())
+
+// RESET count to 0
+
+store.dispatch( {
+  type: 'RESET'
+});
+
+
+// I'd like to reset the count to zero
+
+
+
 console.log(store.getState())
