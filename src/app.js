0f1849714298770e7dba8d4ece21280f4b3d27bd
Terrance <tbarnett2020@gmail.com>
2018-08-31 10:48:51 -0400

    Started Connecting store and Components with React-Redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -14,13 +15,22 @@ store.dispatch(addExpense({description: 'Water bill'}));
 store.dispatch(addExpense({description: 'Gas bill'}));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000);
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
 console.log(visibleExpenses);
 
-console.log(store.getState());
+// Using Provider
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
 ReactDOM.render(
-  <AppRouter />,
+ jsx,
   document.getElementById('app')
 )
