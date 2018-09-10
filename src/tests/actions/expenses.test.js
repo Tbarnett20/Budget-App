ae2e8683cc5f322af9a54a37c6de79bb661a5b42
Terrance <tbarnett2020@gmail.com>
2018-09-10 19:12:44 -0400

    Some testing /Snapshot Testing and Enzyme ***Lost of test files added***

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,18 +1,18 @@
-import { addExpense, editExpense,removeExpense } from '../../actions/expenses'
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
-test ('should setup remove expense action object', () => {
-  const action = removeExpense({id: '123abc'})
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
-    id:'123abc'
+    id: '123abc'
   });
 });
 
-test ('should setup edit expense action object', () => {
-  const action = editExpense('123abc', {note: 'New note value'})
+test('should setup edit expense action object', () => {
+  const action = editExpense('123abc', { note: 'New note value' });
   expect(action).toEqual({
     type: 'EDIT_EXPENSE',
-    id:'123abc',
+    id: '123abc',
     updates: {
       note: 'New note value'
     }
@@ -20,9 +20,9 @@ test ('should setup edit expense action object', () => {
 });
 
 test('should setup add expense action object with provided values', () => {
-  const expenseData = { 
-    description: 'rent',
-    amount: 109500, 
+  const expenseData = {
+    description: 'Rent',
+    amount: 109500,
     createdAt: 1000,
     note: 'This was last months rent'
   };
@@ -30,10 +30,10 @@ test('should setup add expense action object with provided values', () => {
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
     expense: {
-    ...expenseData,
-    id:expect.any(String)
+      ...expenseData,
+      id: expect.any(String)
     }
-  })
+  });
 });
 
 test('should setup add expense action object with default values', () => {
@@ -41,11 +41,11 @@ test('should setup add expense action object with default values', () => {
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
     expense: {
-    id: expect.any(String),
-    description: '',
-    note: '',
-    amount: 0, 
-    createdAt: 0
+      id: expect.any(String),
+      description: '',
+      note: '',
+      amount: 0,
+      createdAt: 0
     }
   });
 });
