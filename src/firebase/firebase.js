fbdcb7b368e8a4a04e4ec2217a3fbb780559c4ca
Terrance Danckert <tbarnett2020@gmail.com>
2018-09-14 15:40:51 -0400

    Fetched data from Firebase***

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,29 +13,55 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Terrance Danckert',
-    age: 24,
-    stressLevel: 7,
-    job: {
-        title: 'Software developer',
-        company: 'Google'
-    },
-    location: {
-      city: 'Morehead',
-      country: 'United States'
-  },
-}).then(() => {
-    console.log('Data is saved!');
-}).catch((e) => {
-    console.log('This failed.' , e);
-});
-
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
+database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
 })
+
+
+
+// Fetching data
+/*
+database.ref()
+  .once('value')
+  .then((snapshot) => {
+    const val = snapshot.val();
+    console.log(val);
+  })
+  .catch((e) => {
+    console.log('Error fetching data', e);
+  })
+ */
+
+
+/*
+ database.ref().set({
+  name: 'Terrance Danckert',
+  age: 24,
+  stressLevel: 7,
+  job: {
+    title: 'Software developer',
+    company: 'Google'
+  },
+  location: {
+    city: 'Morehead',
+    country: 'United States'
+   },
+ }).then(() => {
+     console.log('Data is saved!');
+ }).catch((e) => {
+     console.log('This failed.' , e);
+ });
+  */
+
+  // Updating Data
+/*
+ database.ref().update({
+     stressLevel: 9,
+     'job/company': 'Amazon',
+     'location/city': 'Seattle'
+ })
+ */
 
 // database.ref()
 //  .remove()
