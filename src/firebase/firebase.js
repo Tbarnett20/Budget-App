fdd0d2213735ddfa02dda2ee4e8de3dc7d7357b2
Terrance Danckert <tbarnett2020@gmail.com>
2018-09-15 02:06:03 -0400

    Created seperate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDFstkUxgm5M5upDDYKvWkQBsvJ_hdwGgY",
-    authDomain: "frugally-c4753.firebaseapp.com",
-    databaseURL: "https://frugally-c4753.firebaseio.com",
-    projectId: "frugally-c4753",
-    storageBucket: "frugally-c4753.appspot.com",
-    messagingSenderId: "639527083460"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
 firebase.initializeApp(config);
