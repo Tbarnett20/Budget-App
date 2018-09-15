e760783817a5c5775455d3f72181d8c7e52873d7
Terrance Danckert <tbarnett2020@gmail.com>
2018-09-15 01:23:36 -0400

    Tested Async Redux actions

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -11,9 +11,17 @@ const promise = new Promise((resolve, reject) => {
 console.log('Before')
 
 promise.then((data) => {
-    console.log('1', data);
+  console.log('1', data);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise')
+  }, 50000);
+});
+}).then((str) => {
+  console.log('Does this run?', str)
 }).catch((error)=> {
-console.log('error: ', error);
+  console.log('error: ', error);
 })
 
 console.log('After')
